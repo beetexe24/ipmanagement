61b307cde779cb51c2262839f4ad400d9096d4c1
Reymark <reymark@fasttrackfba.com>
2024-06-07 18:30:27 +0800

    added comment

--- app/Http/Job/Multipletask/sample.php
+++ app/Http/Job/Multipletask/sample.php
@@ -3,5 +3,5 @@ namespace App\Http\Job\Multipletask;
 
 
 class sample  {
-
+    // CLASSES HERE WOULD BE CLOSE FOR MODIFICATION BUT OPEN FOR EXTENSION
 }
--- app/Http/Job/Singletask/sample2.php
+++ app/Http/Job/Singletask/sample2.php
@@ -3,5 +3,5 @@ namespace App\Http\Job\Singletask;
 
 
 class sample2  {
-
+    // DO SINGLE TASK
 }
