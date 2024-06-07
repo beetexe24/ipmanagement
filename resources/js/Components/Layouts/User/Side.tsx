cff4a46243e2ec2fd1fbd5bd73071307b1e74649
Reymark <reymark@fasttrackfba.com>
2024-06-07 17:48:09 +0800

    React folder structure and routing

---
+++ resources/js/Components/Layouts/User/Side.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Side()
+{
+    return (
+        <div>
+            this is side
+
+            
+        </div>
+    );
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>VA Referral</title>
+        <title>IP Address Management</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
